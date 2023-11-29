ca7af892089e6379db32b205b451aef827e0fc9f
Lukas Schwab <lukas.schwab@gmail.com>
2023-11-28 19:06:16 -0800

    Skip linting repeated "set:" directive

--- date/options.go
+++ date/options.go
@@ -10,7 +10,7 @@ import (
 type Options struct {
 	Prompt          string        `help:"Prompt to display" default:"> " env:"GUM_DATE_PROMPT"`
 	PromptStyle     style.Styles  `embed:"" prefix:"prompt." envprefix:"GUM_DATE_PROMPT_"`
-	CursorTextStyle style.Styles  `embed:"" prefix:"cursor." set:"defaultForeground=212" set:"defaultUnderline=true" envprefix:"GUM_DATE_CURSOR_"`
+	CursorTextStyle style.Styles  `embed:"" prefix:"cursor." set:"defaultForeground=212" set:"defaultUnderline=true" envprefix:"GUM_DATE_CURSOR_"` //nolint:staticcheck
 	Value           string        `help:"Initial value in ISO 8601 format, e.g. 2023-11-28" default:""`
 	Header          string        `help:"Header value" default:"" env:"GUM_DATE_HEADER"`
 	HeaderStyle     style.Styles  `embed:"" prefix:"header." set:"defaultForeground=240" envprefix:"GUM_DATE_HEADER_"`
