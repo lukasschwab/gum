dc5db52aca6d341cb1743584831e4063b5d5f687
Lukas Schwab <lukas.schwab@gmail.com>
2023-11-28 17:19:59 -0800

    Style underlined by default

--- date/options.go
+++ date/options.go
@@ -11,7 +11,7 @@ type Options struct {
 	Placeholder     string        `help:"Placeholder value" default:"Type something..." env:"GUM_DATE_PLACEHOLDER"`
 	Prompt          string        `help:"Prompt to display" default:"> " env:"GUM_DATE_PROMPT"`
 	PromptStyle     style.Styles  `embed:"" prefix:"prompt." envprefix:"GUM_DATE_PROMPT_"`
-	CursorTextStyle style.Styles  `embed:"" prefix:"cursor." set:"defaultForeground=212" envprefix:"GUM_DATE_CURSOR_"`
+	CursorTextStyle style.Styles  `embed:"" prefix:"cursor." set:"defaultForeground=212" set:"defaultUnderline=true" envprefix:"GUM_DATE_CURSOR_"`
 	Value           string        `help:"Initial value (can also be passed via stdin)" default:""`
 	Header          string        `help:"Header value" default:"" env:"GUM_DATE_HEADER"`
 	HeaderStyle     style.Styles  `embed:"" prefix:"header." set:"defaultForeground=240" envprefix:"GUM_DATE_HEADER_"`
